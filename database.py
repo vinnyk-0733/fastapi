63a584f27726bfab062670609b30f4e5b502cae7
vinayak_0733 <vinayakumar464@gmail.com>
2026-08-06 17:59:21 +0530

    trying again

--- database.py
+++ database.py
@@ -2,6 +2,6 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-db_url = os.getenv("DATABASE_URL", "postgresql://postgres:vinayakrm073%40@localhost:5432/postgres")
+db_url = os.getenv["DATABASE_URL"]
 engine = create_engine(db_url)
-session = sessionmaker(autoflush=False, autocommit = False, bind=engine)
+session = sessionmaker(autoflush=False, autocommit = False, bind=engine)
