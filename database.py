0879dd59cc88d1912edde69edb722a12f9ef888a
vinayak_0733 <vinayakumar464@gmail.com>
2026-08-06 19:10:52 +0530

    some changes done

--- database.py
+++ database.py
@@ -1,7 +1,11 @@
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
 
-db_url = os.getenv["DATABASE_URL"]
+load_dotenv()
+
+db_url = os.getenv("DATABASE_URL", "sqlite:///./sql_app.db")
 engine = create_engine(db_url)
 session = sessionmaker(autoflush=False, autocommit = False, bind=engine)
+
